770ab89496ee8383de8bdd894536cae7bf66f583
AngelCortes2005 <angelgabriel7098@gmail.com>
2025-07-01 20:40:15 -0600

    Weather Web App Structure

---
+++ app.js
@@ -0,0 +1,31 @@
+async function getWeather(country ){
+    const key = "C62FW4862G8P72BN3M2JBPMWG"
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${country}?key=${key}&contentType=json`;
+
+    const response = await axios.get(url)
+    const data = response.data;
+        
+    const temp = data.currentConditions.temp;
+    const wind = data.currentConditions.windspeed;
+    const condition = data.currentConditions.conditions;
+    const rainProb = data.currentConditions.precipprob;
+
+    document.getElementById("weatherCurrent").innerHTML = `
+    <h2>Current Weather</h2>
+    <p>Temperature: ${temp}°C</p>
+    <p>Wind Speed: ${wind} km/h</p>
+    <p>Rain Probability: ${rainProb}%</p>
+    <p>Condition: ${condition}</p>
+    `;
+
+}
+
+
+document.getElementById("searchBtn").addEventListener("click", async () => {
+    const location = document.getElementById("locationInp").value;
+    if (location !== "") {
+        getWeather(location);
+    } else {
+        alert("Please enter a location.");
+    }
+});
