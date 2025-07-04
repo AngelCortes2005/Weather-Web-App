ebb564dcb59cfa768e952a5b5f17bae262d58c70
AngelCortes2005 <angelgabriel7098@gmail.com>
2025-07-04 16:48:37 -0600

    weather app css changes

--- app.js
+++ app.js
@@ -12,11 +12,25 @@ async function getWeather(country ){
     const rainProb = data.currentConditions.precipprob;
 
     document.getElementById("weatherCurrent").innerHTML = `
-    <h2>Current Weather In ${locationName}</h2>
-    <p>Temperature: ${temp}°C</p>
-    <p>Wind Speed: ${wind} km/h</p>
-    <p>Rain Probability: ${rainProb}%</p>
-    <p>Condition: ${condition}</p>
+    <h2 id="weatherTitle" >Current Weather In ${locationName}</h2>
+        <div id="weatherDit">
+            <div class="weatherInfo">
+                <div class="label">Temperature:</div>
+                <div class="value">${temp}°C</div>
+            </div>
+            <div class="weatherInfo">
+                <div class="label">Wind Speed:</div>
+                <div class="value">${wind} km/h</div>
+            </div>
+            <div class="weatherInfo">
+                <div class="label">Rain Probability:</div>
+                <div class="value">${rainProb}%</div>
+            </div>
+            <div class="weatherInfo">
+                <div class="label">Condition:</div>
+                <div class="value">${condition}</div>
+            </div>
+        </div>
     `;
 
 }
